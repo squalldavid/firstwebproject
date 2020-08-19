228f5140de71084551d81e885e768aabde9d9560
squalldavid <squalldavid@gmail.com>
2020-08-18 23:26:27 -0400

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    
+    <!--<router-view/>-->
+
+    <el-container>
+      <el-header id="header" height="200px">Header</el-header>
+      <el-container>
+        <el-aside id="Left_aside" width="25%">
+          Left_Aside
+          <img src="./assets/logo.png" alt="">
+          <img src="./assets/logo.png" alt="">
+          <img src="./assets/logo.png" alt="">
+          <img src="./assets/logo.png" alt="">
+        </el-aside>
+        <el-main>Main</el-main>
+        <el-aside id="Right_aside" width="25%">Right_Aside</el-aside>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 
@@ -18,6 +34,33 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
 }
+#header
+{
+   background-image: url("./assets/logo.png");
+   background-repeat: no-repeat;
+   background-position: center;
+
+}
+#Left_aside
+{
+  background-color: coral;
+
+}
+#Right_aside
+{
+  background-color: blue;
+
+}
+
+
+*{
+  
+  margin: 0px;
+  padding: 0px;
+}
+
 </style>
